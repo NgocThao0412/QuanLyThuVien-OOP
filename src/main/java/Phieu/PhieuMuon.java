5fae354b02c7bc6491a2c8cfc349941fa06b27da
Phạm Thị Bảo Ny <tranthituyettrinh67890@gmail.com>
2025-10-28 09:17:43 +0000

    add

--- src/main/java/Phieu/PhieuMuon.java
+++ src/main/java/Phieu/PhieuMuon.java
@@ -1,1 +1,47 @@
-Ny
+public class PhieuMuon {
+    private String maphieumuon;
+    private String masach;
+    private String maKH;
+
+    // --- Constructor ---
+    public PhieuMuon(String maphieumuon, String masach, String maKH) {
+        this.maphieumuon = maphieumuon;
+        this.masach = masach;
+        this.maKH = maKH;
+    }
+
+    // --- Getter & Setter ---
+    public String getMaphieumuon() {
+        return maphieumuon;
+    }
+
+    public void setMaphieumuon(String maphieumuon) {
+        this.maphieumuon = maphieumuon;
+    }
+
+    public String getMasach() {
+        return masach;
+    }
+
+    public void setMasach(String masach) {
+        this.masach = masach;
+    }
+
+    public String getMaKH() {
+        return maKH;
+    }
+
+    public void setMaKH(String maKH) {
+        this.maKH = maKH;
+    }
+
+
+    @Override
+    public String toString() {
+        return "PhieuMuon{" +
+                "maphieumuon:'" + maphieumuon + '\'' +
+                ", masach:'" + masach + '\'' +
+                ", maKH:'" + maKH + '\'' +
+                '}';
+    }
+}
--- src/main/java/Phieu/PhieuNhap.java
+++ src/main/java/Phieu/PhieuNhap.java
@@ -1,1 +1,25 @@
-Ny
+public class PhieuNhap {
+    private String maNCC;
+
+    // --- Constructor ---
+    public PhieuNhap(String maNCC) {
+        this.maNCC = maNCC;
+    }
+
+    // --- Getter & Setter ---
+    public String getMaNCC() {
+        return maNCC;
+    }
+
+    public void setMaNCC(String maNCC) {
+        this.maNCC = maNCC;
+    }
+
+   
+    @Override
+    public String toString() {
+        return "PhieuNhap{" +
+                "maNCC:'" + maNCC + '\'' +
+                '}';
+    }
+}
