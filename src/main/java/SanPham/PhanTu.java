104170d5f403b0df493fff0022e5f355b0fa7089
Anh031006 <dangngocminhanh76@gmail.com>
2025-11-15 16:44:54 +0700

    Refactor PhanTu class to include date handling

--- src/main/java/SanPham/PhanTu.java
+++ src/main/java/SanPham/PhanTu.java
@@ -1,2 +1,18 @@
-minh anh //Phần tử ở đây là sách, đừng đổi tên nha
+package SanPham;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public abstract class PhanTu {
+    public static Scanner sc  = new Scanner(System.in);
+    // Tạo tháng năm hiện tại
+    LocalDate localDate = LocalDate.now();
+    int namhientai = localDate.getYear();
+    int thanghientai = localDate.getMonthValue();
+    int ngayhientai = localDate.getDayOfMonth();
+    
+    abstract public void nhap();
+    abstract public void xuat();
+    abstract public void suaThongTin();
+}
 
